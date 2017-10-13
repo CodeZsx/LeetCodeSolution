2d238872e4ff7a7eae4215857fb55c9af3a075fb
codez <786359226@qq.com>
2017-10-13 14:09:04 +0800

    22. Generate Parentheses

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,46 @@
+import com.sun.tools.javac.jvm.Gen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by codez on 2017/10/13.
+ *
+ * 22. Generate Parentheses22. Generate Parentheses
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+public class GenerateParentheses {
+    public static void main(String args[]) {
+        GenerateParentheses g = new GenerateParentheses();
+        System.out.println("result:"+g.generateParenthesis(3));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        next(list, "", 0, 0, n);
+        return list;
+    }
+    public void next(List<String> list, String s, int left, int right, int n){
+        if (s.length() == n * 2) {
+            list.add(s);
+            return;
+        }
+        if (left < n) {
+            next(list, s+"(", left+1, right, n);
+        }
+        if (right < left) {
+            next(list, s+")", left, right+1, n);
+        }
+        return;
+    }
+}
